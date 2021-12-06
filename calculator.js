d8799fe52608f2101c4a62542a1112a23310fbfb
leonardo bulcão <leobc009@gmail.com>
2021-12-06 18:46:15 -0300

    operaçoes incluidas

--- calculator.js
+++ calculator.js
@@ -1,8 +1,21 @@
-
-
+let saveAction
 
 function AddNumber(num){
-    document.getElementById("tela").innerHTML += num
+    let CurrentNumber = document.getElementById("tela").innerHTML
+    if (CurrentNumber.length <= 12){
+        if(CurrentNumber == 0 && !CurrentNumber.includes(".")){
+            document.getElementById("tela").innerHTML = ""
+        } 
+        document.getElementById("tela").innerHTML += num
+    }    
+}
+
+function AddCom(){
+    var CurrentNumber = document.getElementById("tela").innerHTML
+
+    if(!CurrentNumber.includes(".")){
+        document.getElementById("tela").innerHTML += '.'
+    }
 }
 
 function BackSpace(){
@@ -10,55 +23,62 @@ function BackSpace(){
     const novoElemento = elemento.split("")
 
     novoElemento.splice((novoElemento.length -1),1)
-    document.getElementById("tela").innerHTML = novoElemento.join('')
+    if(document.getElementById("tela").innerHTML.length > 1){
+        document.getElementById("tela").innerHTML = novoElemento.join('')
+        
+    } else{
+        document.getElementById("tela").innerHTML = 0
+    }
    
 }
 
 function CleanAll(){
+    document.getElementById("tela").innerHTML = "0"
+    document.getElementById("acumulador").innerHTML = ""
+}
+
+function CleanCurrent(){
+    document.getElementById("tela").innerHTML = "0"
+
+}
+
+function CalcAction(action){
+    let CurrentNumber = document.getElementById("tela").innerHTML
+    saveAction = action
+
+    if(!document.getElementById("acumulador").innerHTML.includes(action)){
+        document.getElementById("acumulador").innerHTML += CurrentNumber + action
+        
+    }
+
+    if(document.getElementById("acumulador").innerHTML.includes('=')){
+        document.getElementById("acumulador").innerHTML = CurrentNumber + action
+    }
+    
     document.getElementById("tela").innerHTML = ""
-}
-
-class operaçao{
-    constructor(operador){
-        this.operador = operador
-    }
-
-    get mostrarNaTela(){
-        return this.operador
-       
-    }
-
-    separar(){
-        const elemento =  document.getElementById("tela").innerHTML
-        const novoElemento = elemento.split(this.operador)
-        return novoElemento
-
-    }
-
-   
-}
-
-class Divirir extends operaçao{
-    constructor(operador){
-        super(operador)
-    }
-
-    operaçao(){
-       const operadores = document.getElementById("tela").innerHTML.separar
-       const resultado = operadores[0]/operadores[1]
-       return resultado
-
-    }
-   
 
 }
 
-function Chamar(operador){
-    document.getElementById("tela").innerHTML += operador.mostrarNaTela
+function result(){
+    let CurrentNumber = document.getElementById("tela").innerHTML
+    const operador1 = Number(document.getElementById("acumulador").innerHTML.replace(saveAction, ""))
+    const operador2 = Number(document.getElementById("tela").innerHTML)
+    const resultado = proccessAction(operador1,operador2, saveAction)
+
+    document.getElementById("acumulador").innerHTML += CurrentNumber + '='
+    document.getElementById("tela").innerHTML = resultado 
+    
+
 }
 
-function operar(){
-
+function proccessAction(num1, num2, action){
+    switch (action){
+        case '+': return num1 + num2 
+        case '-': return num1 - num2
+        case 'x': return num1 * num2
+        case '/': return num1 / num2
+    }
 }
+
 
 CleanAll()
